89f62b9e42a833fa8146fac24a3c280bb5fa5dfc
Julian Korf de Gidts <julian@korfdegidts.nl>
2019-10-28 11:08:06 +0100

    Changed for loadbalancer

--- app.js
+++ app.js
@@ -1,21 +1,58 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+var readline = require('readline')
+const rl = readline.createInterface(process.stdin, process.stdout)
 
-const app = express();
+const app = express()
+
+let server
+let port
+let serverId
+let healthy = true
 
 app.use((req, res, next) => {
-    console.log(req.headers);
-    res.header('Server', 'TestWebsite');
-    next();
+    console.log("\n== New request ==\n")
+    console.log(req.headers)
+    console.log("\n== End of request ==\n")
+    res.header('Server', `Test server with ID: ${serverId}`)
+    next()
 })
 
-app.get('/cacheTest.html', (req, res, next) => {
-    return res.header('Cache-Control', 'Max-Age=10').sendFile(path.join(__dirname, './public', 'cacheTest.html'))
+app.get("/healthy", (req, res) => {
+    return res.send(healthy);
 })
-app.use('/', express.static(__dirname + '/public'));
+
+app.post("/kill", (req, res) => {
+    healthy = false
+    return res.send("Server is unhealthy")
+})
+
+app.post("/revive", (req, res) => {
+    healthy = true
+    return res.send("Server is healthy again")
+})
+
+app.use('/', express.static(__dirname + '/public'))
+
+console.log("Please enter the server details")
+
+rl.setPrompt("Port number > ")
+rl.prompt()
+
+rl.on("line", line => {
+    if (port == undefined) {
+        port = parseInt(line)
+
+        rl.setPrompt("Server ID > ")
+        rl.prompt()
+    }
+    else {
+        serverId = parseInt(line)
+
+        server = app.listen(port, () => {
+            console.log(`game server started on port ${server.address().port}`)
+        })
+    }
 
 
-const server = app.listen(80, () => {
-    console.log(`game server started on port ${server.address().port}`);
-});
+})
 
